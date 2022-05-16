5dc75e603e251bb2e039e7459074079139c8d979
{{ github.actor }} <i-am-zaidali@users.noreply.github.com>
2022-05-16 17:06:38 +0000

    Style Reformatting

--- eventmanager/main.py
+++ eventmanager/main.py
@@ -1,49 +1,50 @@
-import discord
 import asyncio
 import time
-
-from discord.ext import tasks
-from redbot.core import commands, Config
-from redbot.core.bot import Red
-from redbot.core.utils.predicates import MessagePredicate
-from redbot.core.utils.menus import start_adding_reactions
 from typing import Dict, Optional
 
-from .model import Flags, Event
-from .constants import emoji_class_dict, class_spec_dict, Category
+import discord
+from discord.ext import tasks
+from redbot.core import Config, commands
+from redbot.core.bot import Red
+from redbot.core.utils.menus import start_adding_reactions
+from redbot.core.utils.predicates import MessagePredicate
+
+from .constants import Category, class_spec_dict, emoji_class_dict
+from .model import Event, Flags
+
 
 class EventManager(commands.Cog):
     """A cog to create and manage events."""
 
     def __init__(self, bot: Red):
         self.bot = bot
-        self.config = Config.get_conf(
-            self, identifier=0x352567829, force_registration=True
-        )
+        self.config = Config.get_conf(self, identifier=0x352567829, force_registration=True)
         self.config.init_custom("events", 2)
         self.cache: Dict[int, Dict[int, Event]] = {}
-        
+
     async def initialize(self):
         all_guilds = await self.config.custom("events").all()
         for guild_id, guild_config in all_guilds.items():
             g = self.cache.setdefault(int(guild_id), {})
             for event in guild_config.values():
                 g.setdefault(event["message_id"], Event.from_json(self.bot, event))
-                
+
     async def to_cache(self):
         for guild_config in self.cache.values():
             for event in guild_config.values():
                 json = event.json
-                await self.config.custom("events").set_raw(event.guild_id, event.message_id, value=json)
-                
+                await self.config.custom("events").set_raw(
+                    event.guild_id, event.message_id, value=json
+                )
+
     def cog_unload(self):
         asyncio.create_task(self.to_cache())
-        
+
     @commands.command(name="event")
     async def event(self, ctx: commands.Context, *, flags: Flags):
         """
         Start an event.
-        
+
         Valid flags are:
         `--end` - The time the event ends.
         `--image` - The image to use for the event.
@@ -52,52 +53,58 @@ class EventManager(commands.Cog):
         `--channel` - The channel to post the event in. [optional]
         """
         flags["channel_id"] = flags.get("channel_id") or ctx.channel.id
-        event = Event(ctx.bot, message_id=ctx.message.id, guild_id=ctx.guild.id, author_id=ctx.author.id, **flags)
+        event = Event(
+            ctx.bot,
+            message_id=ctx.message.id,
+            guild_id=ctx.guild.id,
+            author_id=ctx.author.id,
+            **flags,
+        )
         msg = await ctx.send(embed=event.embed)
         event.message_id = msg.id
         start_adding_reactions(msg, [i for i in emoji_class_dict.keys()] + ["❌", "🧻"])
         self.cache.setdefault(ctx.guild.id, {})[msg.id] = event
-        
+
     @commands.Cog.listener()
     async def on_member_leave(self, member: discord.Member):
         if member.guild.id not in self.cache:
             return
-        
+
         for event in self.cache[member.guild.id].values():
-            if entrant:=event.get_entrant(member.id):
+            if entrant := event.get_entrant(member.id):
                 event.remove_entrant(entrant)
                 msg = await event.message()
                 if not msg:
                     continue
-                
+
                 await msg.edit(embed=event.embed)
-        
+
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload: discord.RawReactionActionEvent):
         if not payload.guild_id:
             return
-        
-        if not (data:=self.cache.get(payload.guild_id)):
+
+        if not (data := self.cache.get(payload.guild_id)):
             return
-        
-        if not (event:=data.get(payload.message_id)):
+
+        if not (event := data.get(payload.message_id)):
             return
-        
+
         message = await event.message()
-        
+
         if not message:
-            return # idk what could be the reason message is none tbh.
-        
+            return  # idk what could be the reason message is none tbh.
+
         user: Optional[discord.User] = self.bot.get_user(payload.user_id)
-         
+
         if not user:
             return
-        
+
         if user.bot:
             return
-        
+
         emoji = str(payload.emoji)
-        
+
         if not emoji in emoji_class_dict and emoji not in ["❌", "🧻"]:
             try:
                 await message.remove_reaction(emoji, user)
@@ -105,47 +112,50 @@ class EventManager(commands.Cog):
             except Exception:
                 pass
             return
-        
+
         if emoji in emoji_class_dict:
-            
-            if (entrant:=event.get_entrant(user.id)):
+
+            if entrant := event.get_entrant(user.id):
                 emoji_to_remove = class_spec_dict[entrant.category_class]["emoji"]
                 try:
                     await message.remove_reaction(emoji_to_remove, user)
                 except Exception:
                     pass
-            
+
             class_name = emoji_class_dict[emoji]
-            
+
             details = class_spec_dict[class_name]
-            
+
             valid_specs = [(k, v["emoji"]) for k, v in details["specs"].items()]
-            
+
             embed = discord.Embed(
                 title="Select a spec for the class {}".format(class_name),
-                description="\n".join(f"{ind+1}. {spec[1]} {spec[0]}" for ind, spec in enumerate(valid_specs)) + \
-                    "\nSend the correct number to select a spec.",
-                color=discord.Color.green()
+                description="\n".join(
+                    f"{ind+1}. {spec[1]} {spec[0]}" for ind, spec in enumerate(valid_specs)
+                )
+                + "\nSend the correct number to select a spec.",
+                color=discord.Color.green(),
             )
             try:
                 await user.send(embed=embed)
-            
+
             except Exception:
-                await message.channel.send(f"I couldn't dm you to select a spec {user.mention}.\nMake sure your dms are open.")
+                await message.channel.send(
+                    f"I couldn't dm you to select a spec {user.mention}.\nMake sure your dms are open."
+                )
                 return
-            
+
             answer = None
             while answer is None:
                 try:
                     msg = await self.bot.wait_for(
-                        "message", 
-                        check=lambda m: 
-                            m.author == user and
-                            not m.guild and 
-                            m.channel.recipient == user, 
-                        timeout=60
+                        "message",
+                        check=lambda m: m.author == user
+                        and not m.guild
+                        and m.channel.recipient == user,
+                        timeout=60,
                     )
-                    
+
                 except asyncio.TimeoutError:
                     await user.send("You took too long to respond. Cancelling.")
                     try:
@@ -153,28 +163,32 @@ class EventManager(commands.Cog):
                     except Exception:
                         pass
                     return
-                
-                if not msg.content.isdigit() or int(msg.content) not in [i + 1 for i in range(len(valid_specs))]:
-                    await user.send(f"That's not a valid answer. You must write a number from 1 to {len(valid_specs)}")
+
+                if not msg.content.isdigit() or int(msg.content) not in [
+                    i + 1 for i in range(len(valid_specs))
+                ]:
+                    await user.send(
+                        f"That's not a valid answer. You must write a number from 1 to {len(valid_specs)}"
+                    )
                     continue
-            
+
                 answer = int(msg.content)
-            
+
             spec = valid_specs[answer - 1][0]
-            
+
             event.add_entrant(user.id, class_name, details["specs"][spec]["categories"][0], spec)
-            
+
             await user.send("You have been signed up to the event.")
-            
+
             embed = event.embed
-            
+
             await message.edit(embed=embed)
-            
+
             try:
                 await message.remove_reaction(emoji, user)
             except Exception:
                 pass
-            
+
         elif emoji == "❌":
             if not event.author_id == user.id:
                 try:
@@ -182,36 +196,36 @@ class EventManager(commands.Cog):
                 except Exception:
                     pass
                 return
-            
+
             try:
                 await message.clear_reactions()
-                
+
             except Exception:
                 pass
-            
+
             embed = event.end()
-            
+
             await user.send("The event was ended.")
-            
+
             await message.edit(embed=embed)
-            
+
         elif emoji == "🧻":
             try:
                 await message.remove_reaction(emoji, user)
-            
+
             except Exception:
                 pass
-            
-            if entrant:=event.get_entrant(user.id):
+
+            if entrant := event.get_entrant(user.id):
                 event.remove_entrant(entrant)
-            
+
                 await user.send("You have been removed from the event.")
-                
+
                 await message.edit(embed=event.embed)
-            
+
             else:
                 await user.send("You weren't signed up to the event.")
-            
+
     @tasks.loop(seconds=5)
     async def check_events(self):
         for guild_config in self.cache.values():
--- eventmanager/model.py
+++ eventmanager/model.py
@@ -1,20 +1,20 @@
-from argparse import ArgumentParser
 import typing
+from argparse import ArgumentParser
+from datetime import datetime
+
 import dateparser
 import discord
-
-from datetime import datetime
 from redbot.core import commands
 from redbot.core.bot import Red
 from redbot.core.utils import chat_formatting as cf
 
-from .constants import Category, emoji_class_dict, class_spec_dict
+from .constants import Category, class_spec_dict, emoji_class_dict
+
 
 class Event:
-    
     def __init__(
         self,
-        bot: Red, 
+        bot: Red,
         name: str,
         message_id: int,
         channel_id: int,
@@ -24,9 +24,9 @@ class Event:
         end_time: datetime,
         image_url: str,
         description2: str = "",
-        start_time: typing.Optional[datetime] = None
+        start_time: typing.Optional[datetime] = None,
     ) -> None:
-        
+
         self.bot = bot
         self.name = name
         self.guild_id = guild_id
@@ -38,52 +38,52 @@ class Event:
         self.start_time = start_time or datetime.now()
         self.end_time = end_time
         self.image_url = image_url
-        
+
         self.entrants: typing.List[Entrant] = []
-        
+
     @property
     def cog(self):
         return self.bot.get_cog("EventManager")
-        
+
     @property
     def guild(self) -> typing.Optional[discord.Guild]:
         guild = self.bot.get_guild(self.guild_id)
         return guild
-        
+
     @property
     def author(self) -> typing.Optional[discord.Member]:
         return self.guild.get_member(self.author_id)
-    
+
     @property
     def channel(self) -> typing.Optional[discord.TextChannel]:
         return self.guild.get_channel(self.channel_id)
-    
+
     @property
     def message(self):
         return self._get_message
-    
+
     @property
     def embed(self) -> discord.Embed:
         """Create the embed for an event."""
-        
+
         embed = discord.Embed(
             title=self.name,
             description=cf.box(self.description),
             color=discord.Color.green(),
             timestamp=self.start_time,
         )
-        
+
         embed.add_field(
             name="Time",
-            value=f"<t:{int(self.end_time.timestamp())}:F> - <t:{int(self.end_time.timestamp())}:R>", 
-            inline=True
+            value=f"<t:{int(self.end_time.timestamp())}:F> - <t:{int(self.end_time.timestamp())}:R>",
+            inline=True,
         )
-        
+
         joined_melee = [i for i in self.entrants if i.category is Category.MELEE]
         joined_ranged = [i for i in self.entrants if i.category is Category.RANGED]
         joined_healer = [i for i in self.entrants if i.category is Category.HEALER]
         joined_tank = [i for i in self.entrants if i.category is Category.TANK]
-        
+
         if self.entrants:
             for category in Category:
                 ent = [i for i in self.entrants if i.category is category]
@@ -92,32 +92,41 @@ class Event:
                 category_emoji = category.emoji
                 embed.add_field(
                     name=f"{category_emoji} **{category.value}**:  (**{len(ent)}**)",
-                    value="\n".join([f"{class_spec_dict[i.category_class]['specs'][i.spec]['emoji']} {i.user.mention} - <t:{int(i.joined_at.timestamp())}:F>" for i in ent]),
-                    inline=False
+                    value="\n".join(
+                        [
+                            f"{class_spec_dict[i.category_class]['specs'][i.spec]['emoji']} {i.user.mention} - <t:{int(i.joined_at.timestamp())}:F>"
+                            for i in ent
+                        ]
+                    ),
+                    inline=False,
                 )
-                
+
         embed.add_field(
             name=f"**{len(self.entrants)}** Joined Users: ",
-            value=cf.box(f"Melee: {len(joined_melee)}\t Ranged: {len(joined_ranged)}\nHealer: {len(joined_healer)}\t Tank: {len(joined_tank)}"),
-            inline=False
+            value=cf.box(
+                f"Melee: {len(joined_melee)}\t Ranged: {len(joined_ranged)}\nHealer: {len(joined_healer)}\t Tank: {len(joined_tank)}"
+            ),
+            inline=False,
         )
-        
+
         if self.description2:
             embed.add_field(name="Additional Information:", value=self.description2, inline=False)
-        
+
         embed.set_footer(text=f"Created by {self.author}")
         if self.image_url:
             embed.set_image(url=self.image_url)
         return embed
-    
+
     def end(self):
         embed = self.embed
         embed.title = f"Event Ended"
         embed.description = ""
-        embed._fields.insert(0, {"name": self.name, "value": cf.box(self.description), "inline": False})
+        embed._fields.insert(
+            0, {"name": self.name, "value": cf.box(self.description), "inline": False}
+        )
         del self.cog.cache[self.guild_id][self.message_id]
         return embed
-    
+
     @property
     def json(self):
         return {
@@ -133,7 +142,7 @@ class Event:
             "image_url": self.image_url,
             "entrants": [i.json for i in self.entrants],
         }
-    
+
     async def _get_message(self) -> typing.Optional[discord.Message]:
         msg = list(filter(lambda x: x.id == self.message_id, self.bot.cached_messages))
         channel = self.channel or await self.bot.fetch_channel(self.channel_id)
@@ -148,14 +157,14 @@ class Event:
         except Exception:
             msg = None
         return msg
-    
+
     def get_entrant(self, user_id: int) -> typing.Optional["Entrant"]:
         for entrant in self.entrants:
             if entrant.user.id == user_id:
                 return entrant
-            
+
     def add_entrant(self, user_id: int, category_class: str, category: Category, spec: str):
-        if entrant:=self.get_entrant(user_id):
+        if entrant := self.get_entrant(user_id):
             entrant.category_class = category_class
             entrant.category = category
             entrant.spec = spec
@@ -163,10 +172,10 @@ class Event:
             return entrant
         entrant = Entrant(user_id, self, category, category_class, spec, datetime.now())
         self.entrants.append(entrant)
-        
+
     def remove_entrant(self, entrant: "Entrant"):
         self.entrants.remove(entrant)
-    
+
     @classmethod
     def from_json(cls, bot: Red, json: dict) -> "Event":
         json["start_time"] = datetime.fromisoformat(json["start_time"])
@@ -177,19 +186,28 @@ class Event:
         self.entrants = [Entrant.from_json(self, i) for i in entrants]
         return self
 
+
 class Entrant:
-    def __init__(self, user_id: int, event: Event, category: Category, category_class: str, spec: str, joined_at: datetime) -> None:
+    def __init__(
+        self,
+        user_id: int,
+        event: Event,
+        category: Category,
+        category_class: str,
+        spec: str,
+        joined_at: datetime,
+    ) -> None:
         self.user_id = user_id
         self.event = event
-        self.category = category 
-        self.category_class = category_class 
+        self.category = category
+        self.category_class = category_class
         self.spec = spec
         self.joined_at: datetime = joined_at
-        
+
     @property
     def user(self) -> typing.Optional[discord.Member]:
         return self.event.guild.get_member(self.user_id)
-    
+
     @property
     def json(self) -> dict:
         return {
@@ -197,55 +215,105 @@ class Entrant:
             "category": self.category.name,
             "category_class": self.category_class,
             "spec": self.spec,
-            "joined_at": self.joined_at
+            "joined_at": self.joined_at,
         }
-        
+
     @classmethod
     def from_json(cls, event: Event, json: dict):
         json["category"] = Category(json["category"])
         return cls(event=event, **json)
-    
+
+
 class NoExitParser(ArgumentParser):
     def error(self, message):
-        raise commands.BadArgument(message)    
+        raise commands.BadArgument(message)
+
 
 class Flags(commands.Converter):
     async def convert(self, ctx, argument: str):
         argument = argument.replace("—", "--")
         parser = NoExitParser(description="EventManager flag parser", add_help=False)
-        
-        parser.add_argument("--name", "-n", type=str, help="The name of the event.", dest="name", nargs="+", required=True)
-        parser.add_argument("--description", "-d", type=str, help="The description of the event.", dest="description", nargs="+", required=True)
-        parser.add_argument("--description2", "-d2", type=str, help="The description of the event.", dest="description2", nargs="+", default=[])
-        parser.add_argument("--end", "-e", type=str, help="The end time of the event.", dest="end", nargs="+", required=True)
-        parser.add_argument("--image", "-i", type=str, help="The image url of the event.", dest="image", nargs="+", default=[])
-        parser.add_argument("--channel", "-c", type=str, help="The channel to post the event in.", dest="channel", nargs="+")
-        
+
+        parser.add_argument(
+            "--name",
+            "-n",
+            type=str,
+            help="The name of the event.",
+            dest="name",
+            nargs="+",
+            required=True,
+        )
+        parser.add_argument(
+            "--description",
+            "-d",
+            type=str,
+            help="The description of the event.",
+            dest="description",
+            nargs="+",
+            required=True,
+        )
+        parser.add_argument(
+            "--description2",
+            "-d2",
+            type=str,
+            help="The description of the event.",
+            dest="description2",
+            nargs="+",
+            default=[],
+        )
+        parser.add_argument(
+            "--end",
+            "-e",
+            type=str,
+            help="The end time of the event.",
+            dest="end",
+            nargs="+",
+            required=True,
+        )
+        parser.add_argument(
+            "--image",
+            "-i",
+            type=str,
+            help="The image url of the event.",
+            dest="image",
+            nargs="+",
+            default=[],
+        )
+        parser.add_argument(
+            "--channel",
+            "-c",
+            type=str,
+            help="The channel to post the event in.",
+            dest="channel",
+            nargs="+",
+        )
+
         try:
             flags = vars(parser.parse_args(argument.split(" ")))
         except Exception as e:
             raise commands.BadArgument(str(e))
-        
+
         flags["name"] = " ".join(flags["name"])
         flags["description"] = " ".join(flags["description"])
         flags["description2"] = " ".join(flags["description2"])[:1024]
-        
-        
-        if not (time:=dateparser.parse(" ".join(flags["end"]))):
+
+        if not (time := dateparser.parse(" ".join(flags["end"]))):
             raise commands.BadArgument("Invalid end time.")
-        
+
         if time.timestamp() < datetime.now().timestamp():
             raise commands.BadArgument("The end time must be in the future.")
-        
+
         flags["end_time"] = time
-        
+
         flags["image_url"] = " ".join(flags["image"])
-        
-        if chan:=flags.get("channel"):
-            flags["channel_id"] = (await commands.TextChannelConverter().convert(ctx, " ".joint(chan))).id
-            
+
+        if chan := flags.get("channel"):
+            flags["channel_id"] = (
+                await commands.TextChannelConverter().convert(ctx, " ".joint(chan))
+            ).id
+
         del flags["channel"]
         del flags["end"]
         del flags["image"]
-            
+
         return flags
