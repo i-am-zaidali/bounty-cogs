9939a46cdb6dfeedf575db44f782c19030150c33
crayyy_zee <zaidlastid20@gmail.com>
2022-05-28 22:28:14 +0500

    rep lb

--- rep/main.py
+++ rep/main.py
@@ -25,7 +25,7 @@ class RepManager(commands.Cog):
     REMOVE = "removed"
     RESET = "resetted"
     
-    __version__ = "1.0.0"
+    __version__ = "1.0.1"
     __author__ = ["crayyy_zee#2900"]
     
     def __init__(self, bot: Red):
@@ -117,7 +117,7 @@ class RepManager(commands.Cog):
         
     @rep.command(name="add")
     @is_staff()
-    async def rep_add(self, ctx: commands.Context, amount: int, members: commands.Greedy[discord.Member], *, reason: str):
+    async def rep_add(self, ctx: commands.Context, amount: float, members: commands.Greedy[discord.Member], *, reason: str):
         """
         Add a certain amount of reputation to a user.
         """
@@ -135,7 +135,7 @@ class RepManager(commands.Cog):
         
     @rep.command(name="remove")
     @is_staff()
-    async def rep_remove(self, ctx: commands.Context, amount: int, members: commands.Greedy[discord.Member], *, reason: str):
+    async def rep_remove(self, ctx: commands.Context, amount: float, members: commands.Greedy[discord.Member], *, reason: str):
         """
         Remove a certain amount of reputation from a user."""
         
@@ -196,6 +196,31 @@ class RepManager(commands.Cog):
                 
         return await ctx.maybe_send_embed(f"Reset {cf.humanize_list([member.mention for member in success])}'s reputation to 0.\n\n" + (f"{cf.humanize_list([member.mention for member in failed])} had 0 reputation so I couldn't reset them." if failed else ""))
         
+    @rep.command(name="leaderboard", aliases=["lb"])
+    async def rep_lb(self, ctx: commands.Context, amount: typing.Optional[int] = None, reversed=True):
+        """
+        See a ranked list of users with the most/least reputation.
+        
+        The amount if the number of users to show on the leaderboard. (defaults to 10)
+        
+        If the reversed argument is not used, it defaults to True
+        which shows the leaderboard from highest to lowest.
+        Pass False/0 to show the leaderboard from lowest to highest instead.
+        """
+        
+        if amount < 1:
+            return await ctx.maybe_send_embed("You must specify an amount greater than 0.")
+        
+        members = sorted(filter(lambda x: ctx.guild.get_member(x[0]) and x[1], self.cache.get(ctx.guild.id, {}).items()), key=lambda x: x[1], reverse=reversed)[:amount]
+        
+        if not members:
+            return await ctx.maybe_send_embed("There are no users with reputation in this server.")
+        
+        members = [f"**{ind}**. <@{member[0]}>:\t**{cf.humanize_number(member[1])}**" for ind, member in enumerate(members, 1)]
+        
+        h_or_l = "highest" if reversed else "lowest"
+        
+        await ctx.maybe_send_embed(f"***Top {amount} users with {h_or_l} rep***\n\n" + '\n'.join(members))
         
     @commands.group(name="repset", invoke_without_command=True)
     @commands.admin_or_permissions(manage_guild=True)
