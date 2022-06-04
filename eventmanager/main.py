6cc783f9b3540e146eabf4662d2073b502670f30
crayyy_zee <65814383+i-am-zaidali@users.noreply.github.com>
2022-06-04 17:24:12 +0500

    remove events omg

--- eventmanager/main.py
+++ eventmanager/main.py
@@ -308,3 +308,7 @@ class EventManager(commands.Cog):
                         continue
 
                     await msg.edit(embed=embed)
+                    await self.config.custom(
+                            "events", event.guild_id, event.message_id
+                        ).clear()
+                    del self.cache[event.guild_id][event.message_id]
