5207ef576a08bd7b8a09d646848af3c1a4c28282
crayyy_zee <65814383+i-am-zaidali@users.noreply.github.com>
2022-09-13 14:34:03 +0500

    maybe fix?

--- eventmanager/main.py
+++ eventmanager/main.py
@@ -547,11 +547,14 @@ class EventManager(commands.Cog):
                         del self.cache[event.guild_id][event.message_id]
                         continue
 
-                    if (chan_id := await self.config.guild(msg.guild).history_channel()) and (
+                    if (chan_id := await self.config.guild_from_id(event.guild_id).history_channel()) and (
                         chan := msg.guild.get_channel(int(chan_id))
                     ):
                         await chan.send(embed=embed)
-                        await msg.delete()
+                        try:
+                            await msg.delete()
+                        except Excepion:
+                            pass
 
                     else:
                         await msg.edit(embed=embed)
