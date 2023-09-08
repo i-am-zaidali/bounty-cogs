8b450f8eb7c3bbbbd190a44964f900f87574fbef
{{ github.actor }} <i-am-zaidali@users.noreply.github.com>
2023-09-08 18:53:04 +0000

    Style Reformatting

--- stw/main.py
+++ stw/main.py
@@ -38,7 +38,9 @@ class STW(commands.Cog):
         self.config.register_user(inventory={})
 
     async def cog_before_invoke(self, ctx: commands.Context):
-        await ctx.send(f"`Error in command '{ctx.command.qualified_name}'. Check your console or logs for details.`")
+        await ctx.send(
+            f"`Error in command '{ctx.command.qualified_name}'. Check your console or logs for details.`"
+        )
         raise ValueError("Your Mom")
 
     @staticmethod
