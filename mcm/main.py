da844708d240d01001feb9b00afd5a21c9ceeaeb
i-am-zaidali <zaidlastid20@gmail.com>
2023-11-04 13:31:37 +0500

    a few more checks

--- mcm/main.py
+++ mcm/main.py
@@ -33,7 +33,11 @@ lower_str_param = commands.param(converter=str.lower)
 def teacher_check():
     async def predicate(ctx: commands.Context):
         return (
-            ctx.author.get_role(await ctx.cog.config.guild(ctx.guild).course_teacher_role() or -1)
+            await ctx.bot.is_owner(ctx.author)
+            or await ctx.bot.is_mod(ctx.author)
+            or ctx.author.get_role(
+                await ctx.cog.config.guild(ctx.guild).course_teacher_role() or -1
+            )
             is not None
         )
 
